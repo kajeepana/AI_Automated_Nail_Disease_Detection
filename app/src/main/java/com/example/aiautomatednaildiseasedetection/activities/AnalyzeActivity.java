40e0e2ef17d4e1fb23fe8652d41f4f568935706e
Gowry24 <gowry2406@gmail.com>
2026-07-26 16:35:51 +0530

    Create analyze page with image preview and analysis interface

---
+++ app/src/main/java/com/example/aiautomatednaildiseasedetection/activities/AnalyzeActivity.java
@@ -0,0 +1,15 @@
+package com.example.aiautomatednaildiseasedetection.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aiautomatednaildiseasedetection.R;
+
+public class AnalyzeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analyze);
+    }
+}
